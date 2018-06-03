c20bca1d1f77d558e9d162fdd0ddf4826ae1fe42
unknown <unnikrishnan.dn@gmail.com>
2018-06-03 22:36:29 +0530

    Followup

--- src/main/java/com/rainbow/crm/followup/model/Followup.java
+++ src/main/java/com/rainbow/crm/followup/model/Followup.java
@@ -1,6 +1,8 @@
 package com.rainbow.crm.followup.model;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 import com.rainbow.crm.abstratcs.model.CRMBusinessModelObject;
 import com.rainbow.crm.abstratcs.model.CRMModelObject;
@@ -9,6 +11,7 @@ import com.rainbow.crm.company.model.Company;
 import com.rainbow.crm.division.model.Division;
 import com.rainbow.crm.reasoncode.model.ReasonCode;
 import com.rainbow.crm.saleslead.model.SalesLead;
+import com.rainbow.crm.saleslead.model.SalesLeadLine;
 import com.techtrade.rads.framework.annotations.RadsPropertySet;
 import com.techtrade.rads.framework.utils.Utils;
 
@@ -143,6 +146,13 @@ public class Followup extends CRMBusinessModelObject{
 	public void setFollowupDate(Date followupDate) {
 		this.followupDate = followupDate;
 	}
-	
+
+	public Set<SalesLeadLine> getSalesLeadLines()
+	{
+		if (lead !=null)
+			return lead.getSalesLeadLines();
+		else
+			return null;
+	}
 
 }
